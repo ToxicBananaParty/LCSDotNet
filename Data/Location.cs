e51d48f4e6ab2b24a9c788a2139b48a81ddc7644
ryandwyer@gyleegames.com <ryan@rydwy.com>
2020-11-21 23:49:48 -0500

    Delete before move to Unity

--- Data/Location.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LCSDotNet.Data
-{
-    public class Location
-    {
-        private LocationType type;
-        private RentingType renting;
-        private bool hasAmericanFlag, mapped, highSecurity;
-    }
-}
