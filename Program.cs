6b721ed73d747f39db31c07c79b6a7359c47df7b
ryandwyer@gyleegames.com <ryan@rydwy.com>
2020-11-21 20:57:51 -0500

    Create GameManager, core loop

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace LCSDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Game game = new Game();
+        }
+
+        static void SaveGame()
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.NullValueHandling = NullValueHandling.Include;
+        }
+
+        static void LoadGame()
+        {
+            
+        }
+    }
+}
