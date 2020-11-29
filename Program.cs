3bedc4a0c3b848d6310140ee91409b878f56b100
ryandwyer@gyleegames.com <ryan@rydwy.com>
2020-11-29 18:42:44 -0500

    Add random int methods to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using LCSDotNet.Backend;
 using SadConsole;
 using Microsoft.Xna.Framework;
 using Console = SadConsole.Console;
@@ -9,8 +10,22 @@ namespace LCSDotNet
     {
         public static Console console;
         public static readonly int width = 128, height = 32;
+        private static Random rand;
+
+        public static int RandomInt(int max)
+        {
+            return rand.Next(max);
+        }
+
+        public static int RandomInt(int min, int max)
+        {
+            return rand.Next(min, max);
+        }
+        
         static void Main()
         {
+            rand = new Random(DateTime.Now.Millisecond);
+            NameGenerator instance = new NameGenerator(rand);
             // Setup the engine and create the main window.
             SadConsole.Game.Create(width, height);
 
@@ -27,7 +42,7 @@ namespace LCSDotNet
             console = new Console(width, height);
             console.FillWithRandomGarbage();
             console.Fill(new Rectangle(3, 3, 23, 3), Color.Violet, Color.Black, 0, 0);
-            console.Print(4, 4, "Hello from SadConsole");
+            console.Print(4, 4, NameGenerator.Generate(Gender.Male));
 
             Global.CurrentScreen = console;
         }
